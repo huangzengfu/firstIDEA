88478505a6d579298c2ad918d0114c371c4e6c4c
hf18837727153 <516893261@qq.com>
2020-07-31 21:11:10 +0800

    Initial commit

---
+++ src/hf/java/helloworld.java
@@ -0,0 +1,17 @@
+package hf.java;
+
+
+import java.util.ArrayList;
+//这是当前注释
+public class helloworld {
+
+    public static void main(String[] args){
+        System.out.println("你好!!");
+        ArrayList list = new ArrayList();
+    }
+
+    public void method(){
+
+    }
+
+}
