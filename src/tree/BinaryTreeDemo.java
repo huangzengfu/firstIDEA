eff6ef2881ee85b70f067b06ba19c2d985223179
hf18837727153 <516893261@qq.com>
2020-10-29 21:24:41 +0800

    mainshiti

---
+++ src/tree/BinaryTreeDemo.java
@@ -0,0 +1,65 @@
+package tree;
+
+/**
+ * @author hf
+ * @createtime 2020-10-29-20:55
+ **/
+public class BinaryTreeDemo {
+    public static void main(String[] args){
+
+    }
+}
+
+class HeroNode{
+    private int no;
+    private String name;
+    private HeroNode left;
+    private HeroNode right;
+
+    //构造器
+
+    public HeroNode(int no, String name) {
+        this.no = no;
+        this.name = name;
+    }
+
+    public int getNo() {
+        return no;
+    }
+
+    public void setNo(int no) {
+        this.no = no;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public HeroNode getLeft() {
+        return left;
+    }
+
+    public void setLeft(HeroNode left) {
+        this.left = left;
+    }
+
+    public HeroNode getRight() {
+        return right;
+    }
+
+    public void setRight(HeroNode right) {
+        this.right = right;
+    }
+
+    @Override
+    public String toString() {
+        return "HeroNode{" +
+                "no=" + no +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
