3c37bc270031e24cf6aa2def00398ff3ddea1a6b
hf18837727153 <516893261@qq.com>
2020-11-27 21:15:23 +0800

    mainshiti

--- Algorithm/src/kruskal/kruskalCase.java
+++ Algorithm/src/kruskal/kruskalCase.java
@@ -5,7 +5,125 @@ package kruskal;
  * @createtime 2020-11-26-21:18
  **/
 public class kruskalCase {
-    public static void main(String[] args) {
+
+    private int edgeNum;//边的个数
+    private char[] vertexs;//顶点数组
+    private int[][] matrix;//邻接矩阵
+    //使用INF 表示两个顶点不能联通
+    private static final int INF = Integer.MAX_VALUE;
+
+    //构造器
+    public kruskalCase(char[] vertexs, int[][] matrix) {
+        //初始化顶点数和边的个数
+        int vlen = vertexs.length;
+
+        //初始化顶点
+        this.vertexs = new char[vlen];
+        for (int i = 0; i < vlen; i++) {
+            this.vertexs[i] = vertexs[i];
+        }
+        //初始化边
+        this.matrix = new int[vlen][vlen];
+        for (int i = 0; i < vlen; i++) {
+            for (int j = 0; j < vlen; j++) {
+                this.matrix[i][j] = matrix[i][j];
+            }
+        }
+
+        //初始化边长
+        this.edgeNum = 0;
+        for (int i = 0; i < vlen; i++) {
+            for (int j = i+1; j < vlen; j++) {
+                if (matrix[i][j] != INF) {
+                    this.edgeNum++;
+                }
+            }
+        }
 
     }
+
+    //打印邻接矩阵
+    public void print() {
+        System.out.println("打印邻接矩阵~");
+        for (int i = 0; i < vertexs.length; i++) {
+            for (int j = 0; j < vertexs.length; j++) {
+                System.out.printf("%12d\t", matrix[i][j]);
+            }
+            System.out.println();
+        }
+    }
+    //对边进行排序处理
+    public void sortEdges(EData[] edges){
+        for(int i = 0;i<edges.length -1;i++){
+            for (int j = 0;j<edges.length -1-i;j++){
+                if(edges[j].weight > edges[j+1].weight){
+                    EData tmp = edges[j+1];
+                    edges[j+1] = edges[j];
+                    edges[j] = tmp;
+                }
+            }
+        }
+    }
+    //返回vertexs中顶点对应的下标
+    public int getPosition(char ch){
+        for(int i = 0;i<vertexs.length;i++){
+            if(vertexs[i] == ch){
+                return i;
+            }
+        }
+        return -1;
+    }
+    //获取邻接矩阵的边放到EData[]数组中，从matrix邻接矩阵中获取，EData[]形式如[['A','B',12],['B','F',7]]
+    private EData[] getEdges(){
+        int index = 0;
+        EData[] edges = new EData[edgeNum];
+        for(int i = 0;i<vertexs.length;i++){
+            for(int j =i+1;j<vertexs.length;j++){
+                if(matrix[i][j]!=INF){
+                    edges[index++] = new EData(vertexs[i],vertexs[j],matrix[i][j]);
+                }
+            }
+        }
+        return edges;
+    }
+
+    public static void main(String[] args) {
+        char[] vertexs = {'A', 'B', 'C', 'D', 'E', 'F', 'G'};
+        //克鲁斯卡尔算法的邻接矩阵
+        int matrix[][] = {
+                /*A*//*B*//*C*//*D*//*E*//*F*//*G*/
+                /*A*/ {0, 12, INF, INF, INF, 16, 14},
+                /*B*/ {12, 0, 10, INF, INF, 7, INF},
+                /*C*/ {INF, 10, 0, 3, 5, 6, INF},
+                /*D*/ {INF, INF, 3, 0, 4, INF, INF},
+                /*E*/ {INF, INF, 5, 4, 0, 2, 8},
+                /*F*/ {16, 7, 6, INF, 2, 0, 9},
+                /*G*/ {14, INF, INF, INF, 8, 9, 0}};
+        //创建Kruskal实例
+        kruskalCase kruskalCase = new kruskalCase(vertexs,matrix);
+        kruskalCase.print();
+    }
 }
+
+//创建一个类，它的实例对象就是一条边
+class EData{
+    int start;//边的开始节点
+    int end;//边的结束节点
+    int weight;//边的权值
+
+    public EData(int start,int end,int weight){
+        this.start = start;
+        this.end = end;
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return "EData{" +
+                "start=" + start +
+                ", end=" + end +
+                ", weight=" + weight +
+                '}';
+    }
+}
+
