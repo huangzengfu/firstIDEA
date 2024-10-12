7f732285e6a099963f9056eb9e91be4551483762
huangfu <huangfu@qianxin.com>
2024-10-12 17:14:27 +0800

    1、测试git revert2

--- ShuaTi/src/TestGithub.java
+++ ShuaTi/src/TestGithub.java
@@ -6,5 +6,6 @@ public class TestGithub {
     public static void main(String[] args) {
         System.out.println("测试revert ");
         System.out.println("测试revert ");
+        System.out.println("测试revert ");
     }
 }
