208f10922543cd407f6a08618c5d61aabc103379
huangfu <huangfu@qianxin.com>
2024-10-12 16:50:55 +0800

    1、测试 git reset3

--- ShuaTi/src/TestGithub.java
+++ ShuaTi/src/TestGithub.java
@@ -5,5 +5,7 @@
 public class TestGithub {
     public static void main(String[] args) {
         System.out.println();
+        System.out.println("测试git reset");
+        System.out.println("测试git reset");
     }
 }
