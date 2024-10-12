99f887cc6ea68443f6a54fb331126d10b1dc11ba
huangfu <huangfu@qianxin.com>
2024-10-12 17:20:45 +0800

    Revert "1、测试git revert2"
    This reverts commit 7f732285e6a099963f9056eb9e91be4551483762.

--- ShuaTi/src/TestGithub.java
+++ ShuaTi/src/TestGithub.java
@@ -6,6 +6,5 @@ public class TestGithub {
     public static void main(String[] args) {
         System.out.println("测试revert ");
         System.out.println("测试revert ");
-        System.out.println("测试revert ");
     }
 }
